16bf4687f3ca47bbd74a0c81ec202c3900ce6ae9
Matthew Upson <matt@mantisnlp.com>
2021-10-04 10:31:09 +0200

    chg: Remove extraneous code

--- mantis/__version__.py
+++ mantis/__version__.py
@@ -5,13 +5,3 @@ __url__ = "https://github.com/MantisAI/mantis/tree/master"
 __author__ = "MantisAI"
 __author_email__ = "hi@mantisnlp.com"
 __license__ = ""
-
-#
-# Get the API version
-#
-
-try:
-    with open(".api_version", "r") as f:
-        __api_version__ = f.read().strip("\n")
-except Exception:
-    __api_version__ = None
